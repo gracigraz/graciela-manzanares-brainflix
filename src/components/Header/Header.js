ef754cc65e83190ef4e500017e02855095ae1fcb
Graciela Manzanares <gracielamanzanares@MacBook-Pro.local>
2023-07-27 00:38:30 -0400

    updated scss and jsx code for the header  in mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,26 @@
 import './Header.scss';
-
+import logo from '../../assets/images/BrainFlix-logo.svg';
+import profile from '../../assets/images/Mohan-muruge.jpg';
+import upload from '../../assets/images/upload.svg';
+import search from '../../assets/images/search.svg';
 function Header() {
     return (
         <>
-             <header className="header">
-                <img className="header__logo" src={"BrainFlix-logo.svg"} alt="Logo"/>
-                <button type='button'><img src={'../assets/icons/upload.svg'} alt='upload icon'/>UPLOAD</button>
-                <input type="text" placeholder="Search" name="search"></input>
-                <img className="header__avatar" src={"./assests/images/Mohan-muruge.jpg"} alt={"Logo"}/>
+            <header className="header">
+                <img className="header__logo" src={logo} alt="Logo" />
+                <div className='header__div--mobile'>
+                    <div className='header__search-bar' id='searchWrapper'>
+                        <img className="header__search-icon" src={search} alt="Search Icon" />
+                        <input className='header__search-input' type='text' placeholder="Search" name="searchWrapper"></input>
+                    </div>
+                    <img className="header__avatar" src={profile} alt="Mohan Muruge Profile Avatar Image" />
+                </div>
+                
+                <button className='header__button' type='button'>
+                        <img className='header__upload-img' src={upload} alt='upload icon' />
+                        <span>UPLOAD</span>
+                </button>
             </header>
-            
-
         </>
     )
 }
