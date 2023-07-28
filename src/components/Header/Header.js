f1d351cd21946437db9218668138cc76d6eaa9ec
Graciela Manzanares <gracielamanzanares@MacBook-Pro.local>
2023-07-27 22:38:04 -0400

    updated header to be responsive in mobile, tablet, desktop, still in progress

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import './Header.scss';
 import logo from '../../assets/images/BrainFlix-logo.svg';
 import profile from '../../assets/images/Mohan-muruge.jpg';
 import upload from '../../assets/images/upload.svg';
-import search from '../../assets/images/search.svg';
+
 function Header() {
     return (
         <>
@@ -10,16 +10,19 @@ function Header() {
                 <img className="header__logo" src={logo} alt="Logo" />
                 <div className='header__div--mobile'>
                     <div className='header__search-bar' id='searchWrapper'>
-                        <img className="header__search-icon" src={search} alt="Search Icon" />
                         <input className='header__search-input' type='text' placeholder="Search" name="searchWrapper"></input>
                     </div>
                     <img className="header__avatar" src={profile} alt="Mohan Muruge Profile Avatar Image" />
                 </div>
-                
+                <div className='header__search-bar-dt' id='searchWrapper'>
+                        {/* <img className="header__search-icon" src={search} alt="Search Icon" /> */}
+                        <input className='header__search-input' type='text' placeholder="Search" name="searchWrapper"></input>
+                </div>
                 <button className='header__button' type='button'>
                         <img className='header__upload-img' src={upload} alt='upload icon' />
                         <span>UPLOAD</span>
                 </button>
+                <img className="header__avatar-dt" src={profile} alt="Mohan Muruge Profile Avatar Image" />
             </header>
         </>
     )
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+
+import './Hero.scss';
+import logo from '../../assets/images/BrainFlix-logo.svg';
+
+function Hero() {
+    return(
+        <>
+        <div><video poster="https://i.imgur.com/l2Xfgpl.jpg" controls>
+        </video>
+        </div>
+        </>
+    )
+}
+export default Hero;
