97b4a2922d16f08f8ca59f679d1cc26cd62b4b2a
Graciela Manzanares <gracielamanzanares@MacBook-Pro.local>
2023-07-29 15:51:56 -0400

    updated HeroVideoDetails component to include video and description in hero section

--- src/components/HeroVideoDetails/HeroVideoDetails.js
+++ src/components/HeroVideoDetails/HeroVideoDetails.js
@@ -6,26 +6,26 @@ import logo from '../../assets/images/BrainFlix-logo.svg';
 function HeroVideoDetails() {
 
     return (
-        // <section className='hero-video-details'>
+        <section className='hero-video-details'>
             <div className='hero-video'>
                 <video className='hero-video-details__poster' poster="https://i.imgur.com/l2Xfgpl.jpg" controls>
                 </video>
             </div>
-            // <section className="description">
-            //     <h1 className="description__heading">NEXT VIDEOS</h1>
-            //     <div>
-            //         <h4 className="comment__name"></h4>
-            //         <span className="comment__date"></span>
-            //         <img className="next-videos__thumbnail"
-            //             src=""
-            //             alt="" />
-            //         <img className="next-videos__thumbnail"
-            //             src=""
-            //             alt="" />
-            //     </div>
-            //     <p className="description__copy"></p>
-            // </section>
-            // </section>
+            <section className="description">
+                <h1 className="description__heading">NEXT VIDEOS</h1>
+                <div>
+                    <h4 className="comment__name"></h4>
+                    <span className="comment__date"></span>
+                    <img className="next-videos__thumbnail"
+                        src=""
+                        alt="" />
+                    <img className="next-videos__thumbnail"
+                        src=""
+                        alt="" />
+                </div>
+                <p className="description__copy"></p>
+            </section>
+            </section>
     )
 }
 export default HeroVideoDetails;
