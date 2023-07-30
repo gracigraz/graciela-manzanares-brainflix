0359e053dcbc6dc7ba3761e1ebe3e42b538699be
Graciela Manzanares <gracielamanzanares@MacBook-Pro.local>
2023-07-30 15:32:31 -0400

    setting up main for dynamic data rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.scss';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,37 +1,35 @@
 import HeroVideoDetails from '../HeroVideoDetails/HeroVideoDetails';
 import { useState } from 'react';
-import videoDetails from '../../data/video-details.json';
+import videoDetailsData from '../../data/video-details.json';
 import videos from '../../data/videos.json'
 import NextVideos from '../NextVideos/NextVideos';
 import Comments from '../Comments/Comments';
 
 function Main() {
 
-    // //create selectedVideo state variable - initialize to videoDetailsData[0] 1st video in video-details.json
-    // const [selectedVideo, setSelectedVideo] = useState(videoDetailsData[0]);
+    //create selectedVideo state variable - initialize to videoDetailsData[0] 1st video in video-details.json
+    const [selectedVideo, setSelectedVideo] = useState(videoDetailsData[0]);
 
-    // //function to call when a nav thumbnail image is clicked
+    //function to call when a nav thumbnail image is clicked
 
-    // function handleSelectVideo(videoId){
-    //     //find correct video in our videoDetails
-    //     const foundVideo = videoDetailsData.find((video)=>{
-    //         return video.id === videoId;
-    //     })
-    //     //update the selected video state
-    //     setSelectedVideo(foundVideo);
-    // }
+    function handleSelectVideo(videoId){
+        //find correct video in our HeroVideoDetails
+        const foundVideo = videoDetailsData.find((video)=>{
+            return video.id === videoId;
+        })
+        //update the selected video state
+        setSelectedVideo(foundVideo);
+    }
 
-    // //filter the current selected video out of the array that we pass to nav
-    // const filteredVideos = videos.filter((video)=>{ 
-    //     return video.id !==selectedVideo.id;
-    // })
+    //filter the current selected video out of the array that we pass to nav (NextVideos)
+    const filteredVideos = videos.filter((video)=>{ 
+        return video.id !==selectedVideo.id;
+    })
     return (
       <main className= 'video-page'>
-        <HeroVideoDetails /> 
-        {/* selectedVideo = {selectedVideo} */}
+        <HeroVideoDetails selectedVideo = {selectedVideo}/> 
         <Comments />
-        <NextVideos  />
-        {/* filteredVideos = {filteredVideos} handleSelectVideo={handleSelectVideo} */}
+        <NextVideos  filteredVideos = {filteredVideos} handleSelectVideo={handleSelectVideo}/> 
       </main>
     );
   }
